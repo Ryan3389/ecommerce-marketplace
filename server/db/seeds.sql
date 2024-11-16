974e58bb0caae79f07b361ec744ac69dbc286b2d
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-11-16 12:17:08 -0500

    added url to redirect users after payment and updated clear cart button

--- server/db/seeds.sql
+++ server/db/seeds.sql
@@ -7,13 +7,13 @@ VALUES
 
 INSERT INTO products (product_name, product_desc, product_price, product_img, cat_id)
 VALUES 
-('JBL Go 4', 'Ultra-Portable, Waterproof and Dustproof Bluetooth Speaker, 7-Hour Built-in Battery', 69.98, 'monitor.jpg', 1),
+('JBL Go 4', 'Ultra-Portable, Waterproof and Dustproof Bluetooth Speaker, 7-Hour Built-in Battery', 69.98, 'speaker.jpg', 1),
 ('Samsumg Curved Monitor', 'An ultra-slim and sleek profile that measures less than 0.5 inch thick.',329.99, 'monitor.jpg',1),
-('Acana Beef Kibble', 'High in protein, real ingredients', 50.99, 'monitor.jpg', 2),
-('Blue Menu Chicken Kibble', 'Healthy ingredients, high protein', 85.99, 'monitor.jpg', 2),
-('Office Chair', 'Ergonomically Friendly office chair', 150.99, 'monitor.jpg', 3),
-('Recliner chair', 'The most comfortable chair to watch the game in', 100.29, 'monitor.jpg', 3),
-('Rawlings baseball glove', 'Get the latest glove', 135.49, 'monitor.jpg', 4),
-('Bauer hockey stick', 'Latest tech to increase shot speed',225.99, 'monitor.jpg', 4),
-('Baseball bat', 'Get the same bat Aaron Judge uses',150.99, 'monitor.jpg', 4);
+('Acana Beef Kibble', 'High in protein, real ingredients', 50.99, 'acana.webp', 2),
+('Blue Menu Chicken Kibble', 'Healthy ingredients, high protein', 85.99, 'blue.webp', 2),
+('Office Chair', 'Ergonomically Friendly office chair', 150.99, 'chair.jpg', 3),
+('Recliner chair', 'The most comfortable chair to watch the game in', 100.29, 'recliner.webp', 3),
+('Rawlings baseball glove', 'Get the latest glove', 135.49, 'glove.webp', 4),
+('Bauer hockey stick', 'Latest tech to increase shot speed',225.99, 'hockey.webp', 4),
+('Baseball bat', 'Get the same bat Aaron Judge uses',150.99, 'bat.jpg', 4);
 
