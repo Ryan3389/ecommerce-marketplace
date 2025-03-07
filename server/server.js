6049d6672ca8b115569a8848565163ce72b7861e
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-03-06 20:28:14 -0500

    updated server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = require('./routes')
+const path = require('path')
 
 const PORT = process.env.PORT || 3001
 const app = express()
@@ -7,7 +8,13 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+app.use(express.static(path.join(__dirname, '../client/dist')))
+
 app.use(routes)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+})
 
 
 app.listen(PORT, () => {
